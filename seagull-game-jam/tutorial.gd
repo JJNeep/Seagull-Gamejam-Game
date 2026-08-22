c018d12afe93ba6f9c7b4317bfb14e875cab1da5
isaacgaryson-hash <isaac.garyson@gmail.com>
2026-08-22 15:46:48 +0800

    commit

--- seagull-game-jam/tutorial.gd
+++ seagull-game-jam/tutorial.gd
@@ -3,10 +3,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Label.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.visible = true
-	
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Tutorial"):
+		$Label.visible = !$Label.visible
